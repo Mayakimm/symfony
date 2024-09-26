f7954198754fb8f45fc0455d9b11a2181b8b5555
Maya Kim <mayagy.kim@gmail.com>
2024-09-26 17:19:44 +0200

    hashed password

--- cours-de-symfony/tuto_symfony/src/Controller/SigninController.php
+++ cours-de-symfony/tuto_symfony/src/Controller/SigninController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\SigninType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'app_signin')]
-    public function index(Request $req, EntityManagerInterface $entityManager): Response
+    public function index(Request $req, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
 
@@ -24,6 +25,15 @@ class SigninController extends AbstractController
         if($form->isSubmitted() && $form->isValid())
         {
           $user = $form->getData();
+
+          $plaintextPassword = $user->getPassword();
+
+          $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+          );
+
+          $user->setPassword($hashedPassword);
           $entityManager->persist($user);
           $entityManager->flush();
         }
